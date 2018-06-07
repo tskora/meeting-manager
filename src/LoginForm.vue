43ec44e05fdadca8e7a7ecbb155ee090c6c953f4
Tomasz Skora <tskora93@gmail.com>
2018-06-07 08:43:27 +0200

    computed property

--- src/LoginForm.vue
+++ src/LoginForm.vue
@@ -2,18 +2,13 @@
    <div>
        <label>Zaloguj się e-mailem</label>
        <input type="email" v-model="email">
-       <button @click="enter()">{{ buttonLabel }}</button>
+       <button @click="enter()">{{ buttonLabelToDisplay }}</button>
    </div>
 </template>
 
 <script>
 export default {
  props: ['buttonLabel'],
- mounted() {
-    if (!this.buttonLabel) {
-        this.buttonLabel = 'Zaloguj się';
-    }
- },
  data() {
      return {
          email: ''
@@ -22,6 +17,11 @@ export default {
  methods: {
      enter() {
          this.$emit('login', this.email);
+     }
+ },
+ computed: {
+     buttonLabelToDisplay() {
+         return this.buttonLabel || 'Zaloguj się';  
      }
  }
 }
