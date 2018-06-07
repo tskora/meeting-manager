2282ddf97c20d543469ad2f08c25ee6dd57c9d5c
Tomasz Skora <tskora93@gmail.com>
2018-06-07 08:26:38 +0200

    The point after the end of a class

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>Witaj w systemie zapisów na zajęcia</h1>
+
+    <div v-if="!authenticatedUsername">
+      <login-form @login="logMeIn($event)"></login-form>
+    </div>
+    <div v-else>
+      <h2>Witaj {{ authenticatedUsername }}</h2>
+      <button @click="logMeOut()">Wyloguj</button>
+    </div>
+
   </div>
 </template>
 
 <script>
+import "milligram";
+import LoginForm from "./LoginForm";
 export default {
+  components: {LoginForm},
   name: 'app',
   data () {
     return {
-      msg: 'Hello World!!!'
+      authenticatedUsername: ''
+    }
+  },
+  methods: {
+    logMeIn(username) {
+      this.authenticatedUsername = username;
+    },
+    logMeOut() {
+      this.authenticatedUsername = "";
     }
   }
 }
@@ -39,21 +45,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 h1, h2 {
   font-weight: normal;
 }
-
 ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: inline-block;
   margin: 0 10px;
 }
-
 a {
   color: #42b983;
 }
---
+++ src/LoginForm.vue
@@ -0,0 +1,28 @@
+<template>
+   <div>
+       <label>Zaloguj się e-mailem</label>
+       <input type="email" v-model="email">
+       <button @click="enter()">{{ buttonLabel }}</button>
+   </div>
+</template>
+
+<script>
+export default {
+ props: ['buttonLabel'],
+ mounted() {
+    if (!this.buttonLabel) {
+        this.buttonLabel = 'Zaloguj się';
+    }
+ },
+ data() {
+     return {
+         email: ''
+     }
+ },
+ methods: {
+     enter() {
+         this.$emit('login', this.email);
+     }
+ }
+}
+</script>
